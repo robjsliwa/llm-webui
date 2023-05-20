b3dd7dfadcb0802d7ed6601dc856ee4fdb95232b
Rob Sliwa <robjsliwa@gmail.com>
2023-05-20 18:42:59 -0400

    Fixed streaming issues and markdown issues.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ type Message = {
   text: string;
 };
 
+const extractJson = (input: string) => {
+  const match = input.match(/data: (.*?)(\n|$)/s);
+  return match ? JSON.parse(match[1]) : null;
+};
+
 const App: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState("");
@@ -19,11 +24,33 @@ const App: React.FC = () => {
 
   const sendMessage = async () => {
     const prompt = `Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instructions: ${input}\n\n### Response:\n`;
-    // setMessages([
-    //   ...messages,
-    //   { sender: "user", text: input },
-    //   { sender: "bot", text: "" },
-    // ]);
+    setInput("");
+    setLoading(true);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { sender: "user", text: input },
+    ]);
+
+    const response = await fetch("http://192.168.1.163:8000/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt, max_tokens: 1800, stream: false }),
+    });
+
+    const result = await response.json();
+    const answer = result.choices[0].text;
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { sender: "bot", text: answer },
+    ]);
+
+    setLoading(false);
+  };
+
+  const sendMessageWithStreaming = async () => {
+    const prompt = `Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instructions: ${input}\n\n### Response:\n`;
     setInput("");
     setLoading(true);
 
@@ -57,54 +84,33 @@ const App: React.FC = () => {
         const result = decoder.decode(value);
         // console.log(result);
         try {
-          answer += JSON.parse(result.slice(5)).choices[0].text;
+          const cleanedResult = extractJson(result);
+          if (!cleanedResult) {
+            console.log("error", result);
+          }
+          answer += cleanedResult.choices[0].text;
         } catch {
           console.log("error", result);
         }
 
         // escape ordered list that gets removed by markdown
-        answer = answer.replace(/(\d+)\./g, "\n$1) ");
+        // answer = answer.replace(/\n(\d+)\./g, "\n$1) ");
+        answer = answer.replace(/\n/g, "  \n");
         // eslint-disable-next-line no-loop-func
         setMessages((prevMessages) => {
           const updatedMessages = [...prevMessages];
           updatedMessages[botResponseIndex] = { sender: "bot", text: answer };
           return updatedMessages;
         });
-
-        // if (isFirstIteration) {
-        //   console.log("CHECK2", messages);
-        //   // setMessages([
-        //   //   ...messages,
-        //   //   { sender: "user", text: input },
-        //   //   { sender: "bot", text: answer },
-        //   // ]);
-        //   // eslint-disable-next-line no-loop-func
-        //   setMessages((prevMessages) => [
-        //     ...prevMessages,
-        //     { sender: "user", text: input },
-        //     { sender: "bot", text: answer },
-        //   ]);
-        //   isFirstIteration = false;
-        // } else {
-        //   console.log("CHECK1", messages);
-        //   messages[messages.length - 1].text = answer;
-        //   // setMessages([...newMessages]);
-        // }
       }
 
-      // const answer = JSON.parse(result).choices[0].text;
       setLoading(false);
-      // setMessages([
-      //   ...messages,
-      //   { sender: "user", text: input },
-      //   { sender: "bot", text: res },
-      // ]);
     }
   };
 
   return (
     <div className="flex flex-col h-screen p-6 bg-gray-800 text-white">
-      <h1 className="text-4xl font-bold mb-4">ChatGPT Demo</h1>
+      <h1 className="text-4xl font-bold mb-4">Llama Demo</h1>
       <div className="overflow-y-auto mb-4 flex-grow border border-gray-700 p-4 bg-gray-900">
         {messages.map((message, index) => (
           <div
@@ -115,7 +121,11 @@ const App: React.FC = () => {
           >
             <strong className="font-bold">{message.sender}:</strong>
             {message.sender === "bot" ? (
-              <ReactMarkdown children={message.text} />
+              <ReactMarkdown
+                children={message.text}
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[breaks]}
+              />
             ) : (
               <p>{message.text}</p>
             )}
@@ -130,7 +140,7 @@ const App: React.FC = () => {
       />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={sendMessage}
+        onClick={sendMessageWithStreaming}
         disabled={isLoading}
       >
         Send
