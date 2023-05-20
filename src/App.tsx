3d895f37c370c8e31026a18a4db1c49b01ae3fd3
Rob Sliwa <robjsliwa@gmail.com>
2023-05-20 15:03:41 -0400

    Updated look and feel of the chat bot.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import breaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
 
 type Message = {
   sender: "user" | "bot";
@@ -14,8 +17,8 @@ const App: React.FC = () => {
     setInput(e.target.value);
   };
 
-  const sendMessageStreamResponse = async () => {
-    const prompt = `\n\n### Instructions: ${input}\n\n### Response:\n`;
+  const sendMessage = async () => {
+    const prompt = `Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instructions: ${input}\n\n### Response:\n`;
     // setMessages([
     //   ...messages,
     //   { sender: "user", text: input },
@@ -36,18 +39,22 @@ const App: React.FC = () => {
       const reader = response.body.getReader();
       const decoder = new TextDecoder("utf-8");
       let answer = "";
+      let isFirstIteration = true;
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
         const result = decoder.decode(value);
-        console.log(result);
+        // console.log(result);
         try {
           answer += JSON.parse(result.slice(5)).choices[0].text;
         } catch {
           console.log("error", result);
         }
 
-        if (messages.length > 0) {
+        // escape ordered list that gets removed by markdown
+        answer = answer.replace(/(\d+)\./g, "\n$1) ");
+
+        if (messages.length > 0 && !isFirstIteration) {
           messages[messages.length - 1].text = answer;
           setMessages([...messages]);
         } else {
@@ -56,10 +63,8 @@ const App: React.FC = () => {
             { sender: "user", text: input },
             { sender: "bot", text: answer },
           ]);
+          isFirstIteration = false;
         }
-        // console.log(messages[messages.length - 1]);
-        // messages[messages.length - 1].text = answer;
-        // setMessages([...messages]);
       }
 
       // const answer = JSON.parse(result).choices[0].text;
@@ -67,31 +72,40 @@ const App: React.FC = () => {
       // setMessages([
       //   ...messages,
       //   { sender: "user", text: input },
-      //   { sender: "bot", text: answer },
+      //   { sender: "bot", text: res },
       // ]);
     }
   };
 
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-4xl font-bold mb-4">LLama Chat</h1>
-      <div className="overflow-y-auto h-64 mb-4 border p-4">
+    <div className="flex flex-col h-screen p-6 bg-gray-800 text-white">
+      <h1 className="text-4xl font-bold mb-4">ChatGPT Demo</h1>
+      <div className="overflow-y-auto mb-4 flex-grow border border-gray-700 p-4 bg-gray-900">
         {messages.map((message, index) => (
-          <p key={index} className="mb-2">
-            <strong className="font-bold">{message.sender}:</strong>{" "}
-            {message.text}
-          </p>
+          <div
+            key={index}
+            className={`mb-2 p-2 rounded ${
+              message.sender === "user" ? "bg-blue-500" : "bg-green-500"
+            }`}
+          >
+            <strong className="font-bold">{message.sender}:</strong>
+            {message.sender === "bot" ? (
+              <ReactMarkdown children={message.text} />
+            ) : (
+              <p>{message.text}</p>
+            )}
+          </div>
         ))}
       </div>
       <textarea
-        className="w-full h-20 p-2 mb-2 border"
+        className="w-full h-20 p-2 mb-2 border border-gray-700 bg-gray-900 text-white"
         onChange={handleInput}
         value={input}
         disabled={isLoading}
       />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={sendMessageStreamResponse}
+        onClick={sendMessage}
         disabled={isLoading}
       >
         Send
